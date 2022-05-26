657f4d994c0b1c81b694b478eeb0ebbe8a9faeda
0WLB34R <knhrvs@gmail.com>
2022-05-26 14:11:50 -0400

    Task 5 - Paused Contract

---
+++ PausedAndDestroy.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract PausedDestory{
+
+    address private owner;
+    bool public paused;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function setPaused(bool value) public{
+        paused = value;
+    }
+
+    function depositMoney() payable public{
+
+    }
+
+    function withdrawAllMoney() public{
+        require(msg.sender == owner, "Must be the same owner");
+        require(!paused, "Contract must be running");
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function destroyContract(address payable to) public{
+        selfdestruct(to);
+    }
+}
