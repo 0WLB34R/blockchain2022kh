36047a7d7dd50af386049d916bf70952c8fe72e5
0WLB34R <knhrvs@gmail.com>
2022-05-31 12:42:05 -0400

    Task 1 - setMessage Test

---
+++ test/Inbox.test.js
@@ -0,0 +1,17 @@
+const Inbox = artifacts.require('Inbox')
+
+contract("Inbox", accounts =>{
+    it('getMessage', async () =>{
+        const instance = await Inbox.deployed();
+        const message = await instance.getMessage.call();
+        assert.equal(message,"Hi");
+
+    });
+
+    it('setMessage', async () => {
+        const instance = await Inbox.deployed();
+        await instance.setMessage('Hi Ken',{from: accounts[0]});
+        const message = await instance.getMessage.call();
+        assert.equal(message,'Hi Ken')
+    })
+})
