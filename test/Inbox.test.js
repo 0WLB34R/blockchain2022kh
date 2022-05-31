6ecee2c9c9b40ac5c0b8bc80e4ec101b6cb6506b
0WLB34R <knhrvs@gmail.com>
2022-05-31 13:19:59 -0400

    Task 2 - Modifier Test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,10 +8,20 @@ contract("Inbox", accounts =>{
 
     });
 
-    it('setMessage', async () => {
+    it('setMessage should change var', async () => {
         const instance = await Inbox.deployed();
         await instance.setMessage('Hi Ken',{from: accounts[0]});
         const message = await instance.getMessage.call();
         assert.equal(message,'Hi Ken')
+    })
+
+    it('setMessage should not change', async ()=>{
+       try{
+        const instance = await Inbox.deployed();
+        await instance.setMessage('Hi not Ken',{from: accounts[1]});
+       }catch (e){
+        console.log('error',e);
+        assert.equal(e.reason,'Has to be the owner')
+       }
     })
 })
