1c08b569401c335ddf1f6c7e533b90392369829e
0WLB34R <knhrvs@gmail.com>
2022-06-08 12:42:28 -0400

    Task 1 - Before each example

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,8 +1,14 @@
 const Lottery = artifacts.require("Lottery");
 
 contract("Lottery", accounts => {
+
+    let instance
+
+    beforeEach("deploy a contract", async () => {
+        instance = await Lottery.new();
+    })
+
     it("allow one account to enter", async () =>{
-        const instance = await Lottery.deployed()
 
         await instance.enter({from: accounts[1], value: web3.utils.toWei("3","ether")})
 
@@ -11,4 +17,19 @@ contract("Lottery", accounts => {
         assert.equal(accounts[1],players[0])
         assert.equal(1, players.length)
     })
+
+    it("allow entry to multiple accounts", async () =>{
+
+        await instance.enter({from: accounts[0], value: web3.utils.toWei("3","ether")})
+        await instance.enter({from: accounts[1], value: web3.utils.toWei("3","ether")})
+        await instance.enter({from: accounts[2], value: web3.utils.toWei("3","ether")})
+
+        const players = await instance.getPlayers.call();
+
+        assert.equal(accounts[0],players[0])
+        assert.equal(accounts[1],players[1])
+        assert.equal(accounts[2],players[2])
+        assert.equal(3, players.length)
+    })
+
 })
