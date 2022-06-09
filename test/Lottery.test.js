12d0c5f572a701770d1bffc4642967c4bdc1f06b
0WLB34R <knhrvs@gmail.com>
2022-06-09 13:13:04 -0400

    Task 1 - Pick winner and reset array unit test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -51,19 +51,15 @@ contract("Lottery", accounts => {
 
     it("sends money to the winner and resets the player array", async () =>{
 
-        const initialBalanacePlayer = await web3.eth.getBalance(accounts[1])
         await instance.enter({from: accounts[1],value: web3.utils.toWei("5","ether")})
-
-        const initialBalanceSmartContract = await web3.eth.getBalance(instance.address)
-
+        const initialBalanacePlayer = await web3.eth.getBalance(accounts[1])
+        const initialBalanceLottery = await web3.eth.getBalance(instance.address) 
         await instance.pickWinner({from: accounts[0]})
-
-        const finallBalanacePlayer = await web3.eth.getBalance(accounts[1])
-
-        const diff = finallBalanacePlayer-initialBalanceSmartContract
-
-        assert(diff > web3.utils.toWei("4.7","ether"))
-
+        const finalBalancePlayer = await web3.eth.getBalance(accounts[1])
+        const total = parseFloat(initialBalanacePlayer)+parseFloat(initialBalanceLottery)
+        const players = await instance.getPlayers.call()
+        assert.equal(finalBalancePlayer,total)
+        assert.equal(0, players.length)
 
     })
 
