b7c9a2c5c53b47ef3e1fd266ae8b04e179993829
0WLB34R <knhrvs@gmail.com>
2022-06-28 14:12:54 -0400

    Task 3 fix

---
+++ src/ethereum/contracts/ERC20.sol
@@ -0,0 +1,33 @@
+// SPDX-Licence-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./IERC20.sol"
+import "./IERC20Metadata.sol"
+
+contract ERC20 is IERC20, IERC20Metadata {
+    mapping(address => uint256) private _balance;
+    mapping(address => mapping(address => uint256)) private _allowance;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name, string memory symbol){
+        _name = name;
+        _symbol = symbol;
+
+    }
+
+
+    function name() public view override returns(string memory){
+        returns _name;
+    } 
+
+    function symbol() public view override returns(string memory){
+        returns _symbol;
+    } 
+
+    function decimals() public view override returns(uint8){
+        returns 18;
+    } 
