b58b2adad1a0607605846c19f3236adfa64e4a72
0WLB34R <knhrvs@gmail.com>
2022-06-29 15:00:46 -0400

    29/06/2022 - Task 3 - Main contract

--- src/ethereum/contracts/ERC20.sol
+++ src/ethereum/contracts/ERC20.sol
@@ -1,11 +1,12 @@
-// SPDX-Licence-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import "./IERC20.sol"
-import "./IERC20Metadata.sol"
+import "./IERC20.sol";
+import "./IERC20Metadata.sol";
 
 contract ERC20 is IERC20, IERC20Metadata {
-    mapping(address => uint256) private _balance;
+
+    mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
     uint256 private _totalSupply;
@@ -13,104 +14,109 @@ contract ERC20 is IERC20, IERC20Metadata {
     string private _name;
     string private _symbol;
 
-    constructor(string memory name, string memory symbol){
-        _name = name;
-        _symbol = symbol;
-
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
     }
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address owner, address spender, uint256 value);
-    
-    function totalSupply() public view override return(uint256){
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+
+    //event Transfer(address indexed from, address indexed to, uint256 value);
+    //event Approval(address indexed owner, address indexed spender, uint256 value)
+
+
+    function totalSupply() public view override returns (uint256) {
         return _totalSupply;
     }
 
-    function balanceOf(address account) public view override returns(uint256){
-        return _balance[account];
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
     }
 
-   function transfer(address to, uint256 amount) public override returns(bool){
-       address owner = msg.sender;
-       _transfer(owner, to, amount);
-       return true;
-   }
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        address owner = msg.sender;
+        _transfer(owner, to, amount);
+        return true;
+    }
 
-   function allowance(address owner, address spender) public view  override returns(uint256){
-       return _allowances[owner, [spender]];
-   }
+    function allowance(address owner, address spender) public view override  returns (uint256) {
+        return _allowances[owner][spender];
+    }
 
-    function approve(address spender, uint256 amount) public override returns(bool){
+    function approve(address spender, uint256 amount) public override returns (bool) {
         address owner = msg.sender;
         _approve(owner, spender, amount);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) public override returns(bool){
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
         address spender = msg.sender;
-        _spendAllowance(from, to, amount);
-        _transfer(from, to, amount)
+        _spendAllowance(from, spender, amount);
+        _transfer(from, to, amount);
         return true;
     }
-    
-    function name() public view override returns(string memory){
-        returns _name;
-    } 
 
-    function symbol() public view override returns(string memory){
-        returns _symbol;
-    } 
-
-    function decimals() public view override returns(uint8){
-        returns 18;
-    } 
-
-    function _transfer(address from, address to, uint256 amount) internal{
+    function _transfer(address from, address to, uint256 amount) internal {
         require(from != address(0), "ERC20: transfer from the zero address.");
         require(to != address(0), "ERC20: transfer to the zero address.");
-        uint256 fromBalance = _balance[from];
-        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance.");
 
-        unchecked{
-            _balance[from] = fromBalance - amount;
-        }
-            _balance[to] += amount;
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance.");
+    unchecked {
+        _balances[from] = fromBalance - amount;
+    }
+        _balances[to] += amount;
         emit Transfer(from, to, amount);
     }
 
     function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "ERC20: approve from the zero address.");
-        require( spender != address(0), "ERC20: approve to the zero address.");
+        require(spender != address(0), "ERC20: approve to the zero address.");
 
-        _allowances[owner][spender]= amount;
-
+        _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
 
-    function _spendAllowance(address owner, address spender, uint256 amount) internal{
+    function _spendAllowance(address owner, address spender, uint256 amount) internal {
         uint256 currentAllowance = allowance(owner, spender);
-
-        if(currentAllowance != type(uint256).max){
-            require(currentAllowance >= amount, "ERC20: Insufficient allowance");
+        if(currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "ERC20: insufficient allowance.");
+        unchecked {
+            _approve(owner, spender, currentAllowance - amount);
         }
-        unchecked{
-            _approve(owner, spender, currentAllowance-amount);
         }
     }
 
-    function _mint(address account, uint256 amount) internal{
-        require(account != address(0), "ERC20: mint to the zero address");
-        _balance[account] += amount;
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC: mint to the zero address.");
+        _totalSupply += amount;
+        _balances[account] += amount;
         emit Transfer(address(0), account, amount);
     }
 
-    function _burn(address account, uint256 amount) internal{
-        require(account != address(0), "ERC20: burn from zero address.");
-        uint256 accountBalance = _balance[account];
-        require(accountBalance >= amount,"ERC20: burn amount exceeds balance.");
-        _balance[account] = accountBalance - amount;
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20 burn from the zero address.");
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20 burn amount exceeds balance.");
+        _balances[account] = accountBalance - amount;
         _totalSupply -= amount;
-        emit Transfer(account, address(0), amount)
+
+        emit Transfer(account, address(0), amount);
     }
-    
+
+    function increaseTotalSupply(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+
 }
--- src/ethereum/contracts/IER20Metadata.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-Licence-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
-
-interface ERC20Metadata {
-    function name() external view returns(string memory);
-    function symbol() external view returns(string memory);
-    function decimals() external view returns(uint8);
-}
